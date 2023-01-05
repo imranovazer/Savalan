fe9266cdd56b8a24d1f49e121d816ca6fc155e58
imranovazer <57639829+imranovazer@users.noreply.github.com>
2023-01-05 17:06:42 +0400

    changes

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,10 +3,17 @@ import data  from "../data";
 import { Link } from "react-router-dom";
 import image from "../imgs/WINE TOURS.png"
 
+
 export default function MainContent(props)
 {
     const [ActiveCategory, SetActiveCategory] =React.useState(props.activeCat)
-    const InputData = data.categories.find(e=>e.id===ActiveCategory) ;
+    const InputData = data.categories.find(e => e.id === ActiveCategory);
+   
+     function changeCategory(event)
+     {
+         SetActiveCategory(Number(event.target.id))
+         console.log(event.target.id) ;
+     }
     
     return (
         <div className="MainContent">
@@ -26,7 +33,7 @@ export default function MainContent(props)
                     </div>
                 </div>
 
-            <div className="Buttons">
+            {/* <div className="Buttons">
                 <div className="container">
                     {
                         data.categories.map((e)=>
@@ -36,9 +43,73 @@ export default function MainContent(props)
                             ))
                     }
                 </div>
+            </div> */}
+            <div className="Buttons">
+                <div className="container">
+                    {
+                        data.categories.map(e=>
+                            (
+                                <div id={e.id} className={e.id===ActiveCategory?"button active":"button"} onClick={changeCategory}>{e.heading}</div>
+                                
+                            ))
+                    }
+                </div>
             </div>
             <div className="divide-line">
                 <hr/>
+            </div>
+            <div className="vines-per-category">
+                    <div className="select-vine-type">
+                        <span>White wine</span>
+                        <span style={{color : "#BBA05A"}}>Red wine</span>
+                        <span>Rose</span>
+                    </div>
+                    <div className="vines-grid">
+                        {/* //<div className="wines-container"> */}
+                        <div className="vines wines" style={{marginLeft:ActiveCategory===1?"-100%":"0px",height:ActiveCategory===1 && "100px"}}>
+                            {data.categories[0].elements.map(
+                                e=>
+                                (
+                                    <div className="vine">
+                                        <div className="img-container">
+                                            <div className="hover">
+                                                <div className="discover">
+                                                    DISCOVER
+                                                </div>
+                                            </div>
+                                            <img src={e.img}  />
+                                        </div>
+                                        <div className="vine-name">
+                                            {e.name}
+                                        </div>
+
+                                    </div>
+                                )
+                            )}
+                        </div>
+                        <div className="vines distillates">
+                            {data.categories[1].elements.map(
+                                e=>
+                                (
+                                    <div className="vine">
+                                        <div className="img-container">
+                                            <div className="hover">
+                                                <div className="discover">
+                                                    DISCOVER
+                                                </div>
+                                            </div>
+                                            <img src={e.img}  />
+                                        </div>
+                                        <div className="vine-name">
+                                            {e.name}
+                                        </div>
+
+                                    </div>
+                                )
+                            )}
+                        </div>
+                        {/* </div> */}
+                    </div>
             </div>
 
             {/* <div className="vines-per-category">
